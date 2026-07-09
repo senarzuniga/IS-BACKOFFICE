b1b4bc086d1e634b29fe308448805df7ecf57306
senarzuniga <anaekaitz@gmail.com>
2026-07-09 15:15:51 +0200

    fix: use workspace PDF paths in PDFmerge.py

--- informes/ingecart-marketing-kit/Informes/PDFmerge.py
+++ informes/ingecart-marketing-kit/Informes/PDFmerge.py
@@ -1,8 +1,8 @@
 from PyPDF2 import PdfMerger
 
-cover_pdf = r"C:\Users\Inaki Senar\Documents\INGECART\COMMERCIAL\PROYECTOS\PACIFICSOUTH\First page OF110.pdf"
-offer_pdf = r"C:\Users\Inaki Senar\Documents\INGECART\COMMERCIAL\PROYECTOS\PACIFICSOUTH\PCS BHS Corrugator Line OFF-2026-110.pdf"
-output_pdf = r"C:\Users\Inaki Senar\Documents\INGECART\COMMERCIAL\PROYECTOS\PACIFICSOUTH\PSC_Offer_OFF-2026-110_Final.pdf"
+cover_pdf = r"C:\Users\Inaki Senar\Documents\GitHub\IS-BACKOFFICE\informes\ingecart-marketing-kit\Informes\PSC_PROPOSAL_REAL.pdf"
+offer_pdf = r"C:\Users\Inaki Senar\Documents\GitHub\IS-BACKOFFICE\informes\ingecart-marketing-kit\Informes\First page OF110.pdf"
+output_pdf = r"C:\Users\Inaki Senar\Documents\GitHub\IS-BACKOFFICE\informes\ingecart-marketing-kit\Informes\PSC_merged.pdf"
 
 merger = PdfMerger()
 merger.append(cover_pdf)
